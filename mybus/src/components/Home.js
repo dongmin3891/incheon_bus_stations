3043638447d956a0a2235da3ec2469fdbd15e246
dongmin3891 <rlaehdals753@naver.com>
2021-11-09 14:10:12 +0900

    home 렌더 컴포넌트 추가

---
+++ mybus/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BusStation from './BusStation'
+import Header from './Header'
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <BusStation/>
+    </>
+  )
+}
+
+export default Home
