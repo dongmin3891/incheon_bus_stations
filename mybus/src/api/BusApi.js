239f3b04a6a80f84541450840fd5885c33a5f0c3
dongmin3891 <rlaehdals753@naver.com>
2021-11-10 15:17:05 +0900

    원복

--- mybus/src/api/BusApi.js
+++ mybus/src/api/BusApi.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 import * as URLs from '../const'
 // https://test2312.herokuapp.com/
-const busApiUrl = "http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList" //버스위치정보
-const bustRouteApiUrl = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteStationList" //버스노선정보
-const busArrivalApiUrl = "http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalItem"
-const serviceKey = "L%2F1NA9bpn1I%2FXr0C2YPtJGNnZXPyrz3O7o3Hvn8ALqFO9DA40GocvMWlaoq6ofkUhaUTcoGKXmTLhkmTz3bVuA%3D%3D"
-const routeId = "232000089" // 김포 90-1번
-const stationId = "168000413" //마전역
+
 const instance = axios.create({
-  baseURL : `${busApiUrl}?serviceKey=${serviceKey}&routeId=${routeId}`
+  baseURL : ""
 });
 
 instance.interceptors.request.use(
